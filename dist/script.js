e97a58a2047e828f6bd0161793b0d4e3d9f10194
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-05-04 21:45:23 -0500

    pascua-5

--- dist/script.js
+++ dist/script.js
@@ -1,23 +1,16 @@
 // $("#palmSunday").collapse("show");
 
-$("a.pascua-4").addClass("active");
-
 $("a.pascua-2").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
-  $("#pascua-3").addClass("d-none");
-  $("#pascua-4").addClass("d-none");
-  $("#pascua-5").addClass("d-none");
-  $("#pascua-6").addClass("d-none");
+  $(".misic").addClass("d-none");
   $("#pascua-2").removeClass("d-none");
 });
 
 $("a.pascua-3").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
-  $("#pascua-2").addClass("d-none");
-  $("#pascua-4").addClass("d-none");
-  $("#pascua-5").addClass("d-none");
+  $(".misic").addClass("d-none");
   $("#pascua-6").addClass("d-none");
   $("#pascua-3").removeClass("d-none");
 });
@@ -25,9 +18,13 @@ $("a.pascua-3").on("click", function () {
 $("a.pascua-4").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
-  $("#pascua-2").addClass("d-none");
-  $("#pascua-3").addClass("d-none");
-  $("#pascua-5").addClass("d-none");
-  $("#pascua-6").addClass("d-none");
+  $(".misic").addClass("d-none");
   $("#pascua-4").removeClass("d-none");
+});
+
+$("a.pascua-5").on("click", function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+  $(".misic").addClass("d-none");
+  $("#pascua-5").removeClass("d-none");
 });

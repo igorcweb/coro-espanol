af22991cbbc19449edc2234c56a5a59c36d7e886
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-12-06 20:55:42 -0600

    guadalupe

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,20 @@
-$("a.a-2").on("click", function () {
+$("a.a-3").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#a-2").removeClass("d-none");
+  $("#a-3").removeClass("d-none");
 });
 $("a.ic").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
   $("#ic").removeClass("d-none");
+});
+$("a.guadalupe").on("click", function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+  $(".music").addClass("d-none");
+  $("#guadalupe").removeClass("d-none");
 });
 
 

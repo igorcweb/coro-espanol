261ac870c8f16b0035176b3ab333ca84c55d6206
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-12-20 19:46:25 -0600

    misa

--- dist/script.js
+++ dist/script.js
@@ -10,6 +10,12 @@ $("a.navidad").on("click", function () {
   $(".music").addClass("d-none");
   $("#navidad").removeClass("d-none");
 });
+$("a.misa").on("click", function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+  $(".music").addClass("d-none");
+  $("#misa").removeClass("d-none");
+});
 
 
 

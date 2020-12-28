ccc6a1e066a48a4d48f28b14ea38b0cdada1c5d5
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-12-27 18:11:19 -0600

    maria madred de dios

--- dist/script.js
+++ dist/script.js
@@ -1,28 +1,15 @@
-$("a.a-4").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#a-4").removeClass("d-none");
-});
-$("a.navidad").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#navidad").removeClass("d-none");
-});
 $("a.misa").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
   $("#misa").removeClass("d-none");
 });
-$("a.sf").on("click", function () {
+$("a.maria").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#sf").removeClass("d-none");
+  $("#maria").removeClass("d-none");
 });
-
 
 
 

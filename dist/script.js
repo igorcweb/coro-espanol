750c74e9f9a58fc788a09f756617ec36cde9e531
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2021-01-03 19:48:29 -0600

    bautismo

--- dist/script.js
+++ dist/script.js
@@ -4,18 +4,13 @@ $("a.misa").on("click", function () {
   $(".music").addClass("d-none");
   $("#misa").removeClass("d-none");
 });
-$("a.maria").on("click", function () {
+$("a.bautismo").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#maria").removeClass("d-none");
+  $("#bautismo").removeClass("d-none");
 });
-$("a.ep").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ep").removeClass("d-none");
-});
+
 
 
 

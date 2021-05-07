a97d2a782aa24d6a293b57ebfaa059312a3c35cd
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2021-05-06 22:16:05 -0500

    boda

--- dist/script.js
+++ dist/script.js
@@ -4,18 +4,19 @@ $("a.misa").on("click", function () {
   $(".music").addClass("d-none");
   $("#misa").removeClass("d-none");
 });
-$("a.p5").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#p5").removeClass("d-none");
-});
 $("a.p6").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
   $("#p6").removeClass("d-none");
 });
+$("a.boda").on("click", function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+  $(".music").addClass("d-none");
+  $("#boda").removeClass("d-none");
+});
+
 
 
 
